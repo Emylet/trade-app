287a78ecb8a8026d9259ec10231ea7ae1bdaf3da
emylet <mmanueljason@gmail.com>
2022-06-21 05:16:58 -0700

    stuffs

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-const myForm = document.querySelector("#my-form");
-const nameInput = document.querySelector("#name");
-const mailnput = document.querySelector("#email");
-const msg = document.querySelector(".msg");
-const userList = document.querySelector("#users");
 
-
-function logIn(){
-
-    if(nameInput.value ==="mekus" && mailnput.value === "1914" || nameInput.value === "jason" && mailnput.value === "5050") {
-        window.location.href="dashboard.html"
-   }
-   else{
-       msg.innerHTML = "Invalid Input Fields"
-
-       setTimeout(()=> msg.remove(), 10000);
-   }
-    //Clear fields
-    nameInput.value ='';
-    mailnput.value = '';
-    }
-
-    function SignUps(){
-        msg.innerHTML = "Hope you'll adhere to our terms"
-    }
 
     /*
      const li = document.createElement('li');
        li.appendChild(document.createTextNode(`${nameInput.value} : ${mailnput.value}`));
 
        userList.appendChild(li);*/
+      
+       
+
+
+
+
 
 function dropShow() {
     document.getElementById("menuDrops").classList.toggle
