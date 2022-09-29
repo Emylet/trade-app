3db15e9aec3e62906939b70edde142b5ca661380
emylet <mmanueljason@gmail.com>
2022-09-29 15:54:50 +0100

    pushing harder

--- index.js
+++ index.js
@@ -12,17 +12,16 @@
 //var ndsa = window.prompt("enter UserName");
 //var menuInfo =document.getElementById("menuInfo");
 //menuInfo.innerHTML= " Welcome "+ ndsa;
-var adverChange = document.getElementById("nft-advt");
-images = [
-    'advert/Ads5.png','advert/Ads2.png',  'advert/Ads1.png', 'advert/Ads4.png',
- ];
 
-
- setInterval(function() {
-    var imgCount = images.length 
-    var numb = Math.floor(Math.random()* imgCount);
-    adverChange.style.backgroundImage = 'url('+images[numb]+')'
-}, 3000);
+var toggler=document.getElementsByClassName("arrow");
+var i;
+console.log(toggler.length);
+for(i=0;i<toggler.length;i++){
+    toggler[i].addEventListener("click",function(){
+        this.parentElement.querySelector(".more").classList.toggle("show");
+        this.classList.toggle("arrow-down");
+    })
+}
 
 
 function dropShow() {
@@ -30,35 +29,6 @@ function dropShow() {
     ("show");
 }
 
-window.onclick = function(event) {
-    if
-    (!event.target.matches('.menu-btn'))
-    {
-        var dropdowns = document.getElementsByClassName
-        ("menu-contents");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = 
-            dropdowns[i];
-            if
-            (openDropdown.classList.contains('show')) {
-
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
-function darkShow() {
-    document.getElementById("body-bg").style.backgroundColor='black'; 
-    document.getElementById("body-bg").style.color= 'white';
-    document.querySelector(".main").style.color= 'white';  
-}
-function lightShow(){
-    document.getElementById("body-bg").style.backgroundColor='white';
-    document.getElementById("body-bg").style.color= 'black';
-    document.querySelector(".main").style.color= 'darkblue';
-}
 
 function logOut() {
     window.location.href="index.html";
