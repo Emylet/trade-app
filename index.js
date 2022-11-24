98a39a9a39620fab82b310d63b4ed260c0a0661f
emylet <mmanueljason@gmail.com>
2022-11-24 04:00:16 -0800

    Jah bless up

--- index.js
+++ index.js
@@ -13,8 +13,6 @@
 //var menuInfo =document.getElementById("menuInfo");
 //menuInfo.innerHTML= " Welcome "+ ndsa;
 
-<<<<<<< HEAD
-=======
 var toggler=document.getElementsByClassName("arrow");
 var i;
 console.log(toggler.length);
@@ -24,7 +22,6 @@ for(i=0;i<toggler.length;i++){
         this.classList.toggle("arrow-down");
     })
 }
->>>>>>> 33739b4fbd9bf509712a6812bd41053413c8d604
 
 
 function dropShow() {
@@ -32,36 +29,6 @@ function dropShow() {
     ("show");
 }
 
-<<<<<<< HEAD
-window.onclick = function(event) {
-    if
-    (!event.target.matches('.menu-btn'))
-    {
-        var dropdowns = document.getElementsByClassName
-        ("menu-contents");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = 
-            dropdowns[i];
-            if
-            (openDropdown.classList.contains('show')) {
-
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
-function darkShow() {
-    document.getElementById("body-bg").style.backgroundColor='black'; 
-    document.querySelector(".main").style.color= 'white';  
-}
-function lightShow(){
-    document.getElementById("body-bg").style.backgroundColor='white';
-    document.querySelector(".main").style.color= 'darkblue';
-}
-=======
->>>>>>> 33739b4fbd9bf509712a6812bd41053413c8d604
 
 function logOut() {
     window.location.href="index.html";
